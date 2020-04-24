d7ff2d723632629f16cd184ebb321e1fe87db0da
coffee <87863496@qq.com>
2020-04-24 15:22:52 +0800

    安卓中级_课堂练习_2020-04-24

---
+++ app/src/test/java/cn/edu/sdwu/android02/classroom/sn170507180209_02/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cn.edu.sdwu.android02.classroom.sn170507180209_02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
